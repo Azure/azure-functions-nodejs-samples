f5f21a5ecad2a62e0f5a69c383d3d28c4094034a
Eric Jizba <erijiz@microsoft.com>
2024-07-31 11:04:07 -0700

    Update otel samples to use released package

--- ts/src/otelOtlp.ts
+++ ts/src/otelOtlp.ts
@@ -1,6 +1,4 @@
-import { app, LogLevel } from '@azure/functions';
-import { context as otelContext, propagation } from '@opentelemetry/api';
-import { SeverityNumber } from '@opentelemetry/api-logs';
+import { AzureFunctionsInstrumentation } from '@azure/functions-opentelemetry-instrumentation';
 import { getNodeAutoInstrumentations, getResourceDetectors } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
@@ -18,46 +16,8 @@ tracerProvider.register();
 const loggerProvider = new LoggerProvider({ resource });
 loggerProvider.addLogRecordProcessor(new SimpleLogRecordProcessor(new OTLPLogExporter()));
 
-registerInstrumentations({ tracerProvider, loggerProvider, instrumentations: [getNodeAutoInstrumentations()] });
-
-// NOTE: The below code will soon be a part of a new package `@opentelemetry/instrumentation-azure-functions`
-// See here for more info: https://github.com/Azure/azure-functions-nodejs-library/issues/245
-app.setup({ capabilities: { WorkerOpenTelemetryEnabled: true } });
-
-const logger = loggerProvider.getLogger('default');
-app.hook.log((context) => {
-    logger.emit({
-        body: context.message,
-        severityNumber: toOtelSeverityNumber(context.level),
-        severityText: context.level,
-    });
+registerInstrumentations({
+    tracerProvider,
+    loggerProvider,
+    instrumentations: [getNodeAutoInstrumentations(), new AzureFunctionsInstrumentation()],
 });
-
-app.hook.preInvocation((context) => {
-    context.functionHandler = otelContext.bind(
-        propagation.extract(otelContext.active(), {
-            traceparent: context.invocationContext.traceContext.traceParent,
-            tracestate: context.invocationContext.traceContext.traceState,
-        }),
-        context.functionHandler
-    );
-});
-
-function toOtelSeverityNumber(level: LogLevel): SeverityNumber {
-    switch (level) {
-        case 'information':
-            return SeverityNumber.INFO;
-        case 'debug':
-            return SeverityNumber.DEBUG;
-        case 'error':
-            return SeverityNumber.ERROR;
-        case 'trace':
-            return SeverityNumber.TRACE;
-        case 'warning':
-            return SeverityNumber.WARN;
-        case 'critical':
-            return SeverityNumber.FATAL;
-        default:
-            return SeverityNumber.UNSPECIFIED;
-    }
-}
